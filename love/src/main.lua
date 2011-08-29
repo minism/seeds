d0933b6c511d52d3894a5a8d79146a29d187fc30
josh <jbothun@concentricsky.com>
2011-08-29 11:45:48 -0700

    added love skeleton

---
+++ love/src/main.lua
@@ -0,0 +1,27 @@
+require 'math'
+require 'os'
+
+function love.load()
+	math.randomseed(os.time())
+	math.random() -- Dumbass OSX fix
+end
+
+function love.update(dt)
+    --
+end
+
+function love.draw()
+    --
+end 
+
+function love.keypressed(key, unicode)
+    --
+end
+
+function love.mousepressed(x, y, button)
+    --
+end
+
+function love.quit()
+    --
+end
