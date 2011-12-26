a6fed3dfd785836d5f6dc76f810218839595d140
josh <jbothun@concentricsky.com>
2011-12-26 14:05:00 -0800

    incorporated lodaer into main love seed

---
+++ love/loader.lua
@@ -0,0 +1,31 @@
+-- Asset namespaces
+local loader = {}
+
+local progress = 0
+local finished = function() end
+local thread = nil
+
+function loader.load(f)
+    -- Start loading thread
+    finished = f
+    thread = coroutine.create(function()
+        -- Load Stuff
+    end)
+end
+
+function loader.update(dt)
+    if not coroutine.resume(thread) then
+        finished()
+    end
+end
+
+function loader.draw()
+    local padding = 150
+    love.graphics.printf('LOADING', 0, scrh()/2, scrw(), 'center')
+    love.graphics.rectangle('line', padding, scrh() * 2 / 3,
+                                    scrw() - padding * 2, 100)
+    love.graphics.rectangle('fill', padding, scrh() * 2 / 3,
+                                    (scrw() - padding * 2) * progress, 100)
+end
+
+return loader
--- love/main.lua
+++ love/main.lua
@@ -1,31 +1,34 @@
 require 'math'
 require 'os'
+inspect = require 'inspect'
+
+-- Import leaf aliases
+require 'leaf'
+fs      = leaf.fs
+
+-- Global handles
+gameState = {}
+scrw = love.graphics.getWidth
+scrh = love.graphics.getHeight
 
 function love.load()
-	math.randomseed(os.time())
-	math.random() -- Dumbass OSX fix
+    math.randomseed(os.time())
+    math.random() -- Dumbass OSX fix
+
+    -- Start loading sequence
+    local loader = require 'loader'
+    gameState = loader
+    loader.load(function()
+        -- Start game
+        gameState = require 'game'
+    end)
 end
 
-function love.update(dt)
-    --
-end
-
-function love.draw()
-    --
-end 
-
-function love.keypressed(key, unicode)
-    --
-end
-
-function love.mousepressed(x, y, button)
-    --
-end
-
-function love.mousereleased(x, y, button)
-
-end
-
-function love.quit()
-    --
+-- Wrap gamestate functions
+for i, func in ipairs{'update', 'draw', 'keypressed', 'mousepressed', 'mousereleased', 'quit'} do
+    love[func] = function (...)
+        if gameState[func] and type(gameState[func] == 'function') then 
+            gameState[func](...)
+        end
+    end
 end
