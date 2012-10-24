b6b55cfaba090f0c6ca4db7e9050ab2af29d3ee1
josh <jbothun@concentricsky.com>
2012-10-24 08:14:50 -0700

    convert to unix

--- love/conf.lua
+++ love/conf.lua
@@ -1,28 +1,25 @@
 function love.conf(t)
-    -- Custom settings
-    VERSION = '0.0.1'
-    DEBUG   = true
-
-	-- Love settings
-	t.title = "unnamed"
-	t.author = "joshbothun@gmail.com"
-	t.identity = nil
-	t.console = true
-	t.screen.width = 1024
-	t.screen.height = 768
-	t.screen.fullscreen = false
-	t.screen.vsync = true
-	t.screen.fsaa = 0
-	
-	-- Modules
-	t.modules.joystick = true
-	t.modules.audio = true
-	t.modules.keyboard = true
-	t.modules.event = true
-	t.modules.image = true
-	t.modules.graphics = true
-	t.modules.timer = true
-	t.modules.mouse = true
-	t.modules.sound = true   
-	t.modules.physics = false
+    -- Love settings
+    t.title = "unnamed"
+    t.version = "0.8.0"
+    t.author = "joshbothun@gmail.com"
+    t.identity = nil
+    t.console = true
+    t.screen.width = 1024
+    t.screen.height = 768
+    t.screen.fullscreen = false
+    t.screen.vsync = true
+    t.screen.fsaa = 0
+    
+    -- Modules
+    t.modules.joystick = true
+    t.modules.audio = true
+    t.modules.keyboard = true
+    t.modules.event = true
+    t.modules.image = true
+    t.modules.graphics = true
+    t.modules.timer = true
+    t.modules.mouse = true
+    t.modules.sound = true   
+    t.modules.physics = false
 end
